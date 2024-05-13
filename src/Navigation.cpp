86e029261da50a8499ce23320c4e49fe2d36a913
Amine khelif <khelifaminekhelif@gmail.com>
2024-05-13 12:04:03 +0200

    Navigation Argstrings

--- src/Navigation.cpp
+++ src/Navigation.cpp
@@ -3,6 +3,11 @@
 
 namespace travel {
 
+/**
+ * @brief Constructs a Navigation object.
+ * 
+ * @param parser The MetroNetworkParser object used for parsing the metro network.
+ */
 Navigation::Navigation(MetroNetworkParser& parser)
 : metroNetworkParser(parser), connections_hashmap(parser.connections_hashmap) {
     std::cout << "Navigation constructor called" << std::endl;
@@ -17,48 +22,54 @@ Navigation::Navigation(MetroNetworkParser& parser)
     previous.resize(max_node_id + 1, std::numeric_limits<uint64_t>::max());
 }
 
+/**
+ * @brief Computes the shortest path from a given start station to all other stations in the metro network.
+ * 
+ * @param startName The name of the start station.
+ * @param startLine The line of the start station.
+ */
+void Navigation::computeShortestPath(const std::string& startName, const std::string& startLine) 
+{
+    std::fill(distance.begin(), distance.end(), std::numeric_limits<uint64_t>::max());
+    std::fill(previous.begin(), previous.end(), std::numeric_limits<uint64_t>::max());
+    while (!pq.empty())
+        pq.pop(); // Clear the priority queue
+    uint64_t startId = metroNetworkParser.get_station_id_by_name_and_line(startName, startLine);
 
- void Navigation::computeShortestPath(const std::string& startName, const std::string& startLine) 
+    distance[startId] = 0;
+    pq.push(std::make_pair(0, startId));
+
+    while (!pq.empty())
     {
-        std::fill(distance.begin(), distance.end(), std::numeric_limits<uint64_t>::max());
-        std::fill(previous.begin(), previous.end(), std::numeric_limits<uint64_t>::max());
-        while (!pq.empty())
-            pq.pop(); // Clear the priority queue
-        uint64_t startId = metroNetworkParser.get_station_id_by_name_and_line(startName, startLine);
-        
+        uint64_t u = pq.top().second;
+        pq.pop();
 
-        distance[startId] = 0;
-        pq.push(std::make_pair(0, startId));
-
-        while (!pq.empty())
-        {
-            uint64_t u = pq.top().second;
-            // std::cout << "Popping " << metroNetworkParser.get_station_name_by_id(u) << std::endl;
-            pq.pop();
-
-        
         if (connections_hashmap.find(u) != connections_hashmap.end()) {
-            // std::cout << "Connections found for " << metroNetworkParser.get_station_name_by_id(u) << std::endl;
             for (const auto &pair : connections_hashmap.at(u))
             {
-                    uint64_t v = pair.first;
-                    uint64_t w = pair.second;
+                uint64_t v = pair.first;
+                uint64_t w = pair.second;
 
                 if (distance[v] > distance[u] + w)
                 {
-                        distance[v] = distance[u] + w;
-                        previous[v] = u;
-                        pq.push(std::make_pair(distance[v], v));
-                    }
+                    distance[v] = distance[u] + w;
+                    previous[v] = u;
+                    pq.push(std::make_pair(distance[v], v));
+                }
             }
-            }
+        }
         else{
             std::cerr << "Error: Node not found in connections." << std::endl;
         }
-        }
     }
+}
 
-
+/**
+ * @brief Prints the shortest path from a given end station to the start station.
+ * 
+ * @param endName The name of the end station.
+ * @param endLine The line of the end station.
+ */
 void Navigation::printShortestPath(const std::string& endName, const std::string& endLine) const {
     uint64_t endId = metroNetworkParser.get_station_id_by_name_and_line(endName, endLine);
     if (previous[endId] == std::numeric_limits<uint64_t>::max()) {
@@ -78,10 +89,22 @@ void Navigation::printShortestPath(const std::string& endName, const std::string
     std::cout << std::endl;
 }
 
+/**
+ * @brief Gets the shortest distance from the start station to a given end station.
+ * 
+ * @param end The ID of the end station.
+ * @return The shortest distance from the start station to the end station.
+ */
 uint64_t Navigation::getShortestDistance(uint64_t end) const {
     return distance[end];
 }
 
+/**
+ * @brief Gets the shortest path from the start station to a given end station.
+ * 
+ * @param end The ID of the end station.
+ * @return A vector containing the IDs of the stations in the shortest path.
+ */
 std::vector<uint64_t> Navigation::getShortestPath(uint64_t end) const {
     std::vector<uint64_t> path;
     for (uint64_t at = end; at != std::numeric_limits<uint64_t>::max(); at = previous[at]) {
--- src/Navigation.hpp
+++ src/Navigation.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file Navigation.hpp
+ * @brief Contains the declaration of the Navigation class.
+ */
+
 #pragma once
 #ifndef NAVIGATION_HPP
 #define NAVIGATION_HPP
@@ -12,20 +17,52 @@
 namespace travel {
     class MetroNetworkParser;  // Forward declaration
 
+    /**
+     * @class Navigation
+     * @brief Represents a navigation system for a metro network.
+     */
     class Navigation {
     public:
+        /**
+         * @brief Constructs a Navigation object.
+         * @param parser The MetroNetworkParser object used to parse the metro network data.
+         */
         explicit Navigation(MetroNetworkParser &parser);
+
+        /**
+         * @brief Computes the shortest path between two metro stations.
+         * @param startName The name of the starting station.
+         * @param startLine The line of the starting station.
+         */
         void computeShortestPath(const std::string& startName, const std::string& startLine);
+
+        /**
+         * @brief Prints the shortest path between two metro stations.
+         * @param endName The name of the destination station.
+         * @param endLine The line of the destination station.
+         */
         void printShortestPath(const std::string& endName, const std::string& endLine) const;
+
+        /**
+         * @brief Gets the shortest distance between the starting station and a given station.
+         * @param end The ID of the destination station.
+         * @return The shortest distance between the starting station and the destination station.
+         */
         uint64_t getShortestDistance(uint64_t end) const;
+
+        /**
+         * @brief Gets the shortest path between the starting station and a given station.
+         * @param end The ID of the destination station.
+         * @return A vector containing the IDs of the stations in the shortest path.
+         */
         std::vector<uint64_t> getShortestPath(uint64_t end) const;
 
     private:
-        const std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t>>& connections_hashmap;
-        std::vector<uint64_t> distance;
-        std::vector<uint64_t> previous;
-        std::priority_queue<std::pair<uint64_t, uint64_t>, std::vector<std::pair<uint64_t, uint64_t>>, std::greater<std::pair<uint64_t, uint64_t>>> pq;
-        MetroNetworkParser &metroNetworkParser;
+        const std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t>>& connections_hashmap; /**< The hashmap representing the connections between metro stations. */
+        std::vector<uint64_t> distance; /**< The vector storing the shortest distances from the starting station. */
+        std::vector<uint64_t> previous; /**< The vector storing the previous station in the shortest path from the starting station. */
+        std::priority_queue<std::pair<uint64_t, uint64_t>, std::vector<std::pair<uint64_t, uint64_t>>, std::greater<std::pair<uint64_t, uint64_t>>> pq; /**< The priority queue used for Dijkstra's algorithm. */
+        MetroNetworkParser &metroNetworkParser; /**< The MetroNetworkParser object used to parse the metro network data. */
     };
 }
 
